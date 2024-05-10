0ceae8a9b32b97f22f0ef01a6e92734ad9e0faf3
fteywuidok <esjclishabeatty@hotmail.com>
2024-05-10 15:44:47 +0700

    feat

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-const message = "Hello world!";
-const messageLength = message.length;
-console.log(messageLength); // 13
+function decodeString(s) {
+  const stack = [];
+  for (const char of s) {
+    if (char !== "]") stack.push(char);
+    else {
+      let str = "";
+      while (stack.length && stack[stack.length - 1] !== "[")
+        str = stack.pop() + str;
+      stack.pop();
+      let num = "";
+      while (stack.length && !isNaN(stack[stack.length - 1]))
+        num = stack.pop() + num;
+      stack.push(str.repeat(parseInt(num)));
+    }
+  }
+  return stack.join("");
+}
