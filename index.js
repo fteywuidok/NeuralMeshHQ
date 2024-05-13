0839d520a4679c6603c119c2d070331f9218b250
fteywuidok <esjclishabeatty@hotmail.com>
2024-05-13 09:26:49 +0700

    aptos

--- index.js
+++ index.js
@@ -1,34 +1,13 @@
-function solveSudoku(board) {
-  solve(board);
-  function solve(board) {
-    for (let i = 0; i < 9; i++) {
-      for (let j = 0; j < 9; j++) {
-        if (board[i][j] === ".") {
-          for (let num = 1; num <= 9; num++) {
-            const char = num.toString();
-            if (isValid(board, i, j, char)) {
-              board[i][j] = char;
-              if (solve(board)) return true;
-              board[i][j] = ".";
-            }
-          }
-          return false;
-        }
-      }
-    }
-    return true;
+function maxProduct(nums) {
+  if (nums.length === 0) return 0;
+  let maxSoFar = nums[0];
+  let minSoFar = nums[0];
+  let maxProduct = nums[0];
+  for (let i = 1; i < nums.length; i++) {
+    const temp = maxSoFar;
+    maxSoFar = Math.max(nums[i], nums[i] * maxSoFar, nums[i] * minSoFar);
+    minSoFar = Math.min(nums[i], nums[i] * temp, nums[i] * minSoFar);
+    maxProduct = Math.max(maxProduct, maxSoFar);
   }
-  function isValid(board, row, col, char) {
-    for (let i = 0; i < 9; i++) {
-      if (
-        board[row][i] === char ||
-        board[i][col] === char ||
-        board[3 * Math.floor(row / 3) + Math.floor(i / 3)][
-          3 * Math.floor(col / 3) + (i % 3)
-        ] === char
-      )
-        return false;
-    }
-    return true;
-  }
+  return maxProduct;
 }
